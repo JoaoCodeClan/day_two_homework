f5f9ed49b64c55d8f745b3a51fbe198a5a851e51
Joao <johnynecas@hotmail.com>
2017-09-26 17:22:24 +0100

    done short months

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -28,4 +28,42 @@ end
 
 def add_string_as_number(string_1,string_2)
   return string_1.to_i + string_2.to_i
-end 
+end
+
+def number_to_full_month_name(month_number)
+
+ result= case month_number
+  when 1
+ "January"
+  when  2
+  "February"
+  when 3
+     "March"
+  when 4
+     "April"
+  when 5
+     "May"
+  when 6
+     "June"
+  when 7
+     "July"
+  when 8
+     "August"
+  when 9
+     "September"
+  when 10
+     "October"
+  when 11
+     "November"
+  when 12
+     "December"
+  else
+    "Not a month"
+end
+return result
+end
+
+def number_to_short_month_name(month_number)
+  full_month_name = number_to_full_month_name(month_number)
+  result= full_month_name[0..2]
+end
